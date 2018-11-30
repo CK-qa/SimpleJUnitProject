23bd5bf6856bb77ea1aa954fd81fbab2df330c97
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-11-30 16:58:15 +0300

    project reformatted

--- Mod/src/test/java/MainTest.java
+++ Mod/src/test/java/MainTest.java
@@ -1,13 +1,12 @@
-import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
     @Test
     public void sum() {
         Main main = new Main();
-        Assert.assertEquals("1+2 != 3",3, main.sum(1,2));
+        assertEquals("1+2 != 3",3, main.sum(1,2));
     }
 }
